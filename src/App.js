0b87416b8dbf5752edcba63505df76a8df2a691e
Ethan Rose <ethan@pouch.ph>
2022-08-19 16:41:52 -0700

    restyle

--- src/App.js
+++ src/App.js
@@ -5,23 +5,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="logo" style={{maxWidth: '300px'}} />
 
-        <p style={{ width: '50%'}}>
-          <br/>
-          <b>Currently in Closed Beta.</b><br/><br/>
-          If sending international micropayments by the thousands is a pain point for you, drop us a note at <b>hello@pouch.us</b>.
-        </p>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', borderRadius: '20px', backgroundColor: 'rgba(0,0,0,.75)', width: '50%', padding: '40px' }}>
+          <p>
+          
+            <b>Pouch.US Payouts</b>
+            <br/><br/>
+            ⚡<br/><br/>
+            Currently in Closed Beta.
+            <br/><br/>
+            If sending international micropayments by the thousands is a pain point for you, drop us a note at <b>hello@pouch.us</b>.
+          </p>
 
-        <p style={{ width: '50%'}}>
-        Pouch.us is a US-based payment processor focused on improving the efficiency of international micropayments by utilizing the emerging global payment standard: "The Lightning Network".
-        </p>
+          <p>
+          Pouch.us is a US-based payment processor focused on improving the efficiency of international micropayments by utilizing the emerging global payment standard: "The Lightning Network".
+          </p>
+        </div>
 
       </header>
-      <footer style={{backgroundColor: '#EEF', minHeight: '320px', padding: '48px', boxSizing: 'border-box', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-      &copy;2022 Pouch PH Inc
+      <footer style={{backgroundColor: '#0F212E', color: "#FAFAFA", minHeight: '320px', padding: '48px', boxSizing: 'border-box', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', textAlign: 'center'}}>
+      
+      <div>
+        <b>hello@pouch.us</b>
+      </div>
       <br/><br/>
-      hello@pouch.us
+      <div>
+        &copy;2022 Pouch PH Inc
+      </div>
       </footer>
     </div>
   );
